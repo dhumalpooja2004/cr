903bc2cb256633f87dec7665336e83124a30110d
dhumalpooja2004 <dhumalpooja1204@gmail.com>
2025-02-11 11:26:45 +0530

    new chnages

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
-const  WEBSITE_NAME = "Plant Nursary Shop🌱🌵🌱";
+const  WEBSITE_NAME = " Akshay Plant Nursary Shop🌱🌵🌱";
 const WEBSITE_DESCRIPTION =`Welcome To My Plant Nursary🌱🌱🌵,we are a family owned business that has been marketing the community for over 15 years..In Plant Nusary Many Plants are available.A plant nursery business involves growing and selling plants to customers. This can include ornamental plants, fruit trees, herbs, and native species. They include, Sale: Retail nurseries which sell to the general public. Wholesale nurseries, which sell only to businesses such as other nurseries and to commercial gardeners, private nurseries which suffice the needs of institutions or private estates`;
 
 
 const HEADER_IMG = 
-"https://img.freepik.com/free-vector/many-plant-pots-garden-cart-isolated_1308-56103.jpg?ga=GA1.1.1603369098.1719937383&semt=ais_hybrid";
+"https://img.freepik.com/free-photo/houseplants-growing-each-other-room_181624-26512.jpg?ga=GA1.1.1603369098.1719937383&semt=ais_hybrid";
 
 const BUTTON1 = "Explore Plant";
 const BUTTON2 = "Contact Us";
